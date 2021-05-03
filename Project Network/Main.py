2c156c1ed6a1ca81e6ccd037b9f6348250cda3dd
unknown <s.dapratoshepard@gmail.com>
2021-05-03 17:18:49 +0100

    74% accuracy 3 layers 6 epoch

--- Project Network/Main.py
+++ Project Network/Main.py
@@ -34,17 +34,19 @@ def main():
     class Net(nn.Module):
         def __init__(self):
             super().__init__()
-            self.conv1 = nn.Conv2d(3, 12, 5)
+            self.conv1 = nn.Conv2d(3, 64, 3)
+            self.conv2 = nn.Conv2d(64, 128, 3)
+            self.conv3 = nn.Conv2d(128, 256, 3)
             self.pool = nn.MaxPool2d(2, 2)
-            self.conv2 = nn.Conv2d(12, 16, 5)
-            self.fc1 = nn.Linear(16 * 5 * 5, 120)
-            self.fc2 = nn.Linear(120, 84)
-            self.fc3 = nn.Linear(84, 10)
+            self.fc1 = nn.Linear(64 * 4 * 4, 128)
+            self.fc2 = nn.Linear(128, 256)
+            self.fc3 = nn.Linear(256, 10)
 
         def forward(self, x):
             x = self.pool(F.relu(self.conv1(x)))
             x = self.pool(F.relu(self.conv2(x)))
-            x = x.view(-1, 16 * 5 * 5)
+            x = self.pool(F.relu(self.conv3(x)))
+            x = x.view(-1, 64 * 4 * 4)
             x = F.relu(self.fc1(x))
             x = F.relu(self.fc2(x))
             x = self.fc3(x)
@@ -55,7 +57,7 @@ def main():
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-    for epoch in range(2):  # loop over the dataset multiple times
+    for epoch in range(6):  # loop over the dataset multiple times
 
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
@@ -87,8 +89,8 @@ def main():
     images, labels = dataiter.next()
 
     # print images
-    imshow(torchvision.utils.make_grid(images))
-    print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+    #imshow(torchvision.utils.make_grid(images))
+    #print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
     net = Net()
     net.load_state_dict(torch.load(PATH))
